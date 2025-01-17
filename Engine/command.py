56b4a2e3fe00d0626e0cb81e97ca5b8305cbd935
Pratham269 <pshimpi26@gmail.com>
2025-01-17 21:58:25 +0530

    listning and responce to user

--- Engine/command.py
+++ Engine/command.py
@@ -1,13 +1,39 @@
 import pyttsx3
+import speech_recognition as sr
+
 
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices') 
     engine.setProperty('voice', voices[1].id)
-    engine.setProperty('rate', 174)
+    engine.setProperty('rate', 159)
     print(voices)
     engine.say(text)
     engine.runAndWait()
 
-speak("Welcome master piyush, I know you can do it, I am Always there with you")
+
+
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('recognizing')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+        
+
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+text = takecommand()
+
+speak(text)
