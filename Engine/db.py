16a9784556eefa4799f731edeeaab2bcdf66caf5
Pratham269 <pshimpi26@gmail.com>
2025-02-20 23:15:38 +0530

    Whatsapp Automation

--- Engine/db.py
+++ Engine/db.py
@@ -23,18 +23,32 @@ cursor = con.cursor()
 #cursor.execute(delete_query, (row_id,))
 #con.commit()
 
-# Create a table with the desired columns
-cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+# # Create a table with the desired columns
+# cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
 
-desired_columns_indices = [0, 30]
+# desired_columns_indices = [0, 3]
 
-# Read data from CSV and insert into SQLite table for the desired columns
-with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
-    csvreader = csv.reader(csvfile)
-    for row in csvreader:
-        selected_data = [row[i] for i in desired_columns_indices]
-        cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+# # Read data from CSV and insert into SQLite table for the desired columns
+# with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         selected_data = [row[i] for i in desired_columns_indices]
+#         cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
 
-# Commit changes and close connection
-con.commit()
-con.close()
+# # Commit changes and close connection
+# con.commit()
+# con.close()
+
+# #### 4. Insert Single contacts (Optional)
+
+# query = "INSERT INTO contacts VALUES (null,'pawan', '1234567890')"
+# cursor.execute(query)
+# con.commit()
+
+# #### 5. Search Contacts from database
+# query = 'kunal'
+# query = query.strip().lower()
+
+# cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+# results = cursor.fetchall()
+# print(results[0][0])
